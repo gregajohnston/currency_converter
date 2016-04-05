52ef1a8fead773ae0138882cbd8b8915179d424e
Greg Johnston <greg.a.johnston@gmail.com>
2016-04-04 21:44:57 -0400

    More changes, finished first draft of converter.

--- currency.py
+++ currency.py
@@ -1,4 +1,5 @@
-import Math
+import math
+
 
 class Currency:
 
@@ -8,12 +9,25 @@ class Currency:
             self.value = 1.0
         elif len(args) == 1:
             self.code = args[0][0:1]
+            self.code = change_from_symbol(self.code)
             self.value = args[0][1:]
+            self.value = float(self.value.strip())
         elif len(args) == 2:
             self.code = args[0]
             self.value = args[1]
         else:
             raise TypeError("Must import 0, 1 or 2 arguments")
+
+    def change_from_symbol(string):
+        if string == '$':
+            string = 'USD'
+        elif string == '€':
+            string = 'EUR'
+        elif string == '£':
+            string = 'GBP'
+        elif string == "¥":
+            string = "JPY"
+        return string
 
     def __eq__(self, other):
         if isinstance(other, self.__class__):
@@ -27,7 +41,7 @@ class Currency:
     def __add__(self, other):
         if isinstance(other, self.__class__):
             if self.code == other.code:
-                return self.value + other.value
+                return Currency(self.code, self.value + other.value)
             else:
                 raise DifferentCurrencyCodeError("Cannot add different currencies")
         raise ValueError("Both variables not type Currency")
@@ -35,15 +49,14 @@ class Currency:
     def __sub__(self, other):
         if isinstance(other, self.__class__):
             if self.code == other.code:
-                return self.value - other.value
+                return Currency(self.code, self.value - other.value)
             else:
                 raise DifferentCurrencyCodeError("Cannot add different currencies")
         raise ValueError("Both variables not type Currency")
 
     def __mul__(self, number):
         if type(number) == int or type(number) == float:
-            self.value = self.value * other.value
-            return self
+            return Currency(self.code, self.value * number)
         else:
             raise ValueError("Must multiple by a int or float")
         raise ValueError("Requires two arguments, a currency and an int (or float)")
--- currency_converter.py
+++ currency_converter.py
@@ -1,19 +1,35 @@
 class CurrencyConverter:
-    def __init__(self, rate_dict={'USD': 1, 'EUR': 0.88}):
-        self.rate_dict = rate_dict
 
-    def convert(self, currency, code):
-        if code == currency.code:
-            return_currency = currency
-            return return_currency
-        if code != currency.code:
-            return_currency = Currency(code, currency.value)
-            return return_currency * rate_dict[code]
+    def __init__(self, exchange_rate={'USD': 1, 'EUR': 0.88}):
+        self.exchange_rate = exchange_rate
+
+    def default_exchange_rates(self):
+        self.exchange_rate = {'USD': 1.0,
+                            'EUR' : 0.87774
+                            'GBP' : 0.70077
+                            'INR' : 66.1158
+                            'AUD' : 1.31447
+                            'CAD' : 1.30651}
 
 
-    # rate_dict={'USD': 1.00,
-    #         'EUR' : 0.87774
-    #         'GBP' : 0.70077
-    #         'INR' : 66.1158
-    #         'AUD' : 1.31447
-    #         'CAD' : 1.30651}
+    def convert(self, from_currency, to_code):
+        if (to_code not in exchange_rate.keys() or
+            from_currency.code not in exchange_rate.keys()):
+            raise UnknownCurrencyCodeError("Currency is missing. Cannot convert.")
+        elif from_currency.code == to_code:
+            to_currency = from_currency
+            return to_currency
+        else:
+            to_currency = Currency(to_code,
+                            (from_currency.value *
+                            self.exchange_rate[to_code] /
+                            self.exchange_rate[from_code]))
+            return to_currency
+
+
+class UnknownCurrencyCodeError(Exception):
+
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
